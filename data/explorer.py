41a9259e22cee634dbeb5787a16e4976cfc6c4a3
parker57 <parker57@uni.coventry.ac.uk>
2018-04-16 08:27:44 +0100

    added basic explorer

---
+++ data/explorer.py
@@ -0,0 +1,24 @@
+import json
+
+with open('data.json') as json_file:
+	foods = json.load(json_file)
+
+national_origins = {}
+
+for food in foods:
+	for country in food['countries_en'].split(','):
+		if country not in national_origins:
+			national_origins[country] = 1
+		else:
+			national_origins[country] += 1
+
+print(len(foods), 'foods in json file.')
+
+chinese_meals = national_origins['China'] + national_origins['Hong Kong'] + national_origins['Singapore']
+british_meals = national_origins['United Kingdom']
+
+print(chinese_meals, 'of Chinese origin.')
+print(british_meals, 'of British origin.')
+
+
+#print(national_origins)
