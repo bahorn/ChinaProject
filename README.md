e7381185f6e91eec73f96678fbd4194f65227ede
parker57 <parker57@uni.coventry.ac.uk>
2018-04-18 08:03:53 +0100

    adding a venv

---
+++ README.md
@@ -0,0 +1,4 @@
+# China project
+
+## Cultural reccomendations using machine learning
+## Cuttently with an emphasis on food
--- data/explorer.py
+++ data/explorer.py
@@ -1,28 +1,54 @@
+#Full Chinglish
 import json
 
+#English Name, Chinese Name, Image, price?, flvours?
+
+
 with open('data.json') as json_file:
-	foods = json.load(json_file)
+    foods = json.load(json_file)
 
 national_origins = {}
 
 for food in foods:
-	for country in food['countries_en'].split(','):
-		if country not in national_origins:
-			national_origins[country] = 1
-		else:
-			national_origins[country] += 1
+    for country in food['countries_en'].split(','):
+        if country not in national_origins:
+            national_origins[country] = 1
+        else:
+            national_origins[country] += 1
 
 print(len(foods), 'foods in json file.')
 
 chinese_meals = national_origins['China'] + national_origins['Hong Kong'] + national_origins['Singapore']
 british_meals = national_origins['United Kingdom']
 
-print(chinese_meals, 'of Chinese origin.')
-print(british_meals, 'of British origin.')
+#can take a string or list of strings
+def foods_from(origin_countries):
+    national_foods = []
+    for food in foods:
+        for country in food['countries_en'].split(','):
+            if country in origin_countries:
+                national_foods.append(food)
+    return national_foods
 
-for food in foods:
-	for country in food['countries_en'].split(','):
-		if country in ['China','Hong Kong','Singapore']:
-			print(food)
+chinese_foods = foods_from(['China','Hong Kong','Singapore'])
+british_foods = foods_from('United Kingdom')
 
+print(len(chinese_foods), 'of Chinese origin.')
+print(len(british_foods), 'of British origin.')
+
+##for food in foods:
+##    for country in food['countries_en'].split(','):
+##        if country in ['China','Hong Kong','Singapore']:
+##            chinese_foods.append(food)
+
+#Will show all non ingredient keys
+
+#print all tags
+##def get_tags():
+##    for key in foods[0]:
+##        if '100g' not in key:
+##            print(key)
 #print(national_origins)
+
+for i in range(50):
+    print(british_foods[i]['product_name'])
